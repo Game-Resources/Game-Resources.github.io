aee958ad744c7c6cc95b46f96a719ea9b6d04d6b
moton-03 <kirari.moton@outlook.com>
2023-02-17 21:25:28 +0900

    Update

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-function ShowGameList() {
-    document.getElementById("mainiframe").src = "Game/"
-}
-function ShowAbout() {
-    document.getElementById("mainiframe").src = "About/"
-}
-function ShowNews() {
-    document.getElementById("mainiframe").src = "https://game-resources.github.io/Post/categories/#general"
-}
